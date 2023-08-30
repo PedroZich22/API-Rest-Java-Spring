f61eafd7a46bd790c78b6399d95100fe51d25701
Pedro Zich <zichpedro22@gmail.com>
2023-08-30 17:49:57 -0300

    feat: CRUD methods

---
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -0,0 +1,53 @@
+package com.example.crud.controllers;
+
+import com.example.crud.domain.product.Product;
+import com.example.crud.domain.product.ProductRepository;
+import com.example.crud.domain.product.RequestProductDTO;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @GetMapping
+    public ResponseEntity getAllProducts(){
+        var allProducts = repository.findAll();
+        return ResponseEntity.ok(allProducts);
+    }
+
+    @PostMapping
+    public ResponseEntity registerProduct(@RequestBody @Valid RequestProductDTO data) {
+        Product newProduct = new Product(data);
+        repository.save(newProduct);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity updateProduct(@RequestBody @Valid RequestProductDTO data) {
+        Optional<Product> optionalProduct = repository.findById(data.id());
+        if(optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            product.setName(data.name());
+            product.setPrice_in_cents(data.price_in_cents());
+            return ResponseEntity.ok(product);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteProduct(@PathVariable String id) {
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/example/crud/domain/product/Product.java
@@ -0,0 +1,25 @@
+package com.example.crud.domain.product;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name="product")
+@Entity(name="product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private Integer price_in_cents;
+
+    public Product(RequestProductDTO data){
+        this.name = data.name();
+        this.price_in_cents = data.price_in_cents();
+    }
+}
